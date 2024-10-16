64c5a4027f372210fa02463f5a7f5d9de7e3c7b7
FelixCarrier <129521661+Padaf3t@users.noreply.github.com>
2024-10-16 14:58:48 -0400

    Corriger erreur console out of bound #4

--- Assets/Scripts/StateMachine/PlayerState/HurtState.cs
+++ Assets/Scripts/StateMachine/PlayerState/HurtState.cs
@@ -5,22 +5,37 @@ using UnityEngine.Windows;
 
 public class HurtState : State
 {
-    public Transform pocket;
+    public GameObject pocket;
     /// <summary>
     /// Drop percentage only takes values between 0.01 to 0.99
     /// </summary>
     private float dropPercentage = 0.33f;
+    private int numberCatnipRelease;
+    private int amountOfCatnipDropped;
 
     public override void Enter()
     {
-        ReleaseCatnip();
-        isComplete = true;
+        numberCatnipRelease = 0;
+
+        int catnipCount = pocket.transform.childCount;
+
+        amountOfCatnipDropped = (int)(catnipCount * dropPercentage);
+
+        if (catnipCount < 3)
+        {
+            amountOfCatnipDropped = catnipCount;
+        }
     }
 
     public override void Do()
     {
-
-
+        if(numberCatnipRelease == amountOfCatnipDropped)
+        {
+            isComplete = true;
+        }
+        else { 
+            ReleaseCatnip(); 
+        }
     }
 
     public override void FixedDo()
@@ -34,28 +49,18 @@ public class HurtState : State
 
     private void ReleaseCatnip()
     {
-        int catnipCount = pocket.transform.childCount;
+        Vector2 direction = new Vector2((float)Random.Range(-0.5f, 0.5f), (float)Random.Range(1, 2));
+        float force = (float)Random.Range(0.5f, 2);
 
-        int amountOfCatnipDropped = (int)(pocket.transform.childCount * dropPercentage);
+        Transform catnip = pocket.transform.GetChild(0);
+        catnip.position = pocket.transform.position;
+        catnip.SetParent(null);
+        catnip.gameObject.SetActive(true);
+        catnip.GetComponent<CatnipScript>().AsReappear();
 
-        if (catnipCount < 3)
-        {
-            amountOfCatnipDropped = catnipCount;
-        }
+        Rigidbody2D catnipRb = catnip.GetComponent<Rigidbody2D>();
+        catnipRb.velocity = direction * force;
 
-        for (int i = 0; i < amountOfCatnipDropped; i++)
-        {
-            Vector2 direction = new Vector2((float)Random.Range(-0.5f, 0.5f), (float)Random.Range(1, 2));
-            float force = (float)Random.Range(0.5f, 2);
-
-            Transform catnip = pocket.transform.GetChild(i);
-            catnip.position = pocket.transform.position;
-            catnip.SetParent(null);
-            catnip.gameObject.SetActive(true);
-            catnip.GetComponent<CatnipScript>().AsReappear();
-
-            Rigidbody2D catnipRb = catnip.GetComponent<Rigidbody2D>();
-            catnipRb.velocity = direction * force;
-        }
+        numberCatnipRelease++;
     }
 }
